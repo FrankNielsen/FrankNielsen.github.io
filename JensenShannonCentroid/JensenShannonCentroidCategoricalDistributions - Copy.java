4fcf5cd884b78fab18ba9ecf823bbded6536878e
FrankNielsen <frank_nielsen@yahoo.com>
2023-02-01 17:18:14 +0900

    w

--- JensenShannonCentroid/JensenShannonCentroidCategoricalDistributions - Copy.java
+++ JensenShannonCentroid/JensenShannonCentroidCategoricalDistributions - Copy.java
@@ -1,21 +1,12 @@
-// (C) Dec. 2019, All rights reserved by Frank Nielsen, Frank.Nielsen@acm.org
-/*
-This program implements the convex-concave procedure for finding the Jensen-Shannon centroid
- of a set of n histograms.
- 
- See: On a Generalization of the Jensen?Shannon Divergence and the Jensen?Shannon Centroid
- by Frank Nielsen
- Entropy 2020, 22(2), 221; https://doi.org/10.3390/e22020221
- https://www.mdpi.com/1099-4300/22/2/221
- 
- */
+// Frank.Nielsen@acm.org
+// (C) Dec. 2019, All rights reserved
 
 class JensenShannonCentroidCategoricalDistribution
 {
 
   //
   // Draw a normalized histogram or mutinoulli distribution
-  //
+  //	
   public static double [] drawRandom(int d)
   {
     int i;
@@ -23,13 +14,41 @@ class JensenShannonCentroidCategoricalDistribution
     double [] res=new double[d];
 
     for (i=0; i<d; i++) {
-      res[i]=Math.random();
+      res[i]=Math.random(); 
       csum+=res[i];
+    }	
+    for (i=0; i<d; i++) res[i]/=csum;	
+
+    return res;
+  }	
+
+  public static double alphaKL(double  alpha, double [] p, double [] q)
+  {
+    double res=0.0;
+    int i;
+    int d=p.length;
+
+    for (i=0; i<d; i++)  
+    {
+      res+=p[i]*Math.log(p[i]/((1.0-alpha)*p[i]+alpha*q[i]));
     }
-    for (i=0; i<d; i++) res[i]/=csum;
+
 
     return res;
   }
+
+
+  public static double alphaJS(double  alpha, double [] p, double [] q)
+  { 
+    return 0.5*(alphaKL(alpha, p, q)+alphaKL(alpha, q, p));
+  }
+
+  public static double min(double a, double b)
+  {
+    if (a<b) return a; 
+    else return b;
+  }
+ 
 
   public static  double hcross( double p[], double[] q)
   {
@@ -39,23 +58,69 @@ class JensenShannonCentroidCategoricalDistribution
       res+= p[i]*Math.log(q[i]);
 
     return -res;
-  }
+  }    
 
   public static  double h( double p[])
   {
     return hcross(p, p);
-  }
+  }    
 
 
   public static  double KL ( double p[], double[] q)
-  {
+  { 
     return hcross(p, q)-h(p);
-  }
+  }   
 
 
 
+  public static  double hcrossPos( double p[], double[] q)
+  {
+    int d=p.length;
+    double   res=0;
+    for (int i=0; i<d; i++)
+      res+= p[i]*Math.log(q[i])+q[i];
 
-  // Linear interpolation
+    return -res;
+  }    
+
+
+
+  public static  double hPos( double p[])
+  {
+    return hcrossPos(p, p);
+  }    
+
+
+  public static  double KLPos ( double p[], double[] q)
+  { 
+    return hcrossPos(p, q)-hPos(p);
+  }   
+
+
+  public static  double KLPoscf ( double p[], double[] q)
+  {
+    int d=p.length;
+    double   res=0;
+    for (int i=0; i<d; i++)
+      res+= p[i]*Math.log(p[i]/q[i])+q[i]-p[i];
+
+    return res;
+  }    
+
+
+
+  public static  double KLcf ( double p[], double[] q)
+  {
+    int d=p.length;
+    double   res=0;
+    for (int i=0; i<d; i++)
+      res+= p[i]*Math.log(p[i]/q[i]);
+
+    return res;
+  }    
+
+
+
   public static  double [] LERP (double a, double p[], double[] q)
   {
     int d=p.length;
@@ -67,13 +132,30 @@ class JensenShannonCentroidCategoricalDistribution
   }
 
 
+  public static  double K (double a, double p[], double[] q)
+  { 
+    return KL(p, LERP(a, p, q));
+  }
+
+  public static  double JS (double a, double p[], double[] q)
+  {
+    return 0.5*(K(a, p, q)+K(a, q, p));
+  }
+
+
+  public static  double JSs (double a, double p[], double[] q)
+  {
+    return 0.5*(KL(LERP(a, p, q), LERP(0.5, p, q))+KL(LERP(1-a, p, q), LERP(0.5, p, q)));
+  }
+
 
   public static  double JS (double p[], double[] q)
   {
+
     return 0.5*KL(p, LERP(0.5, p, q))+  0.5*KL(q, LERP(0.5, p, q));
   }
 
-  // Objective function to minimize
+
   public static  double JSsetavg (double [][] p, double[] q)
   {
     int n=p.length;
@@ -82,13 +164,14 @@ class JensenShannonCentroidCategoricalDistribution
 
     for (i=0; i<n; i++)
     {
+      //res+=0.5*KL(p[i], LERP(0.5, p[i], q))+  0.5*KL(q, LERP(0.5, p[i], q));
       res+=JS(p[i], q);
     }
 
     return res/(double)n;
   }
 
-  // center of mass of the histograms
+
   public static  double [] CenterMass (double [][] p)
   {
     int n=p.length;
@@ -109,7 +192,7 @@ class JensenShannonCentroidCategoricalDistribution
     return res;
   }
 
-  // primal parameter of a mixture family parameter, drop last coordinate
+  // primal parameter of a mixture family
   public static  double [] Lambda2Theta(double [] lambda)
   {
     int D=lambda.length-1;
@@ -163,7 +246,7 @@ class JensenShannonCentroidCategoricalDistribution
     return res;
   }
 
-  // Jensen divergence
+
   public static  double J(double [] theta1, double [] theta2)
   {
     return 0.5*(F(theta1)+F(theta2))-F(HalfVector(theta1, theta2));
@@ -184,12 +267,12 @@ class JensenShannonCentroidCategoricalDistribution
       res[i]=Math.log(theta[i]/(1.0-csum)) ;
     }
 
-
+ 
     return res;
   }
 
 
-  // gradF^{-1}
+
   public static  double [] gradG (double [] eta)
   {
     int D=eta.length;
@@ -243,29 +326,30 @@ class JensenShannonCentroidCategoricalDistribution
     for (i=0; i<n; i++) {
       thetaset[i]=Lambda2Theta(p[i]);
     }
-    // initialize to center of mass
+
+    //res=CenterMass(p);
     theta=CenterMass(thetaset);
     res=Theta2Lambda(theta);
 
     double loss=JSsetavg(p, res);
 
-    System.out.println("Initial objective function for histogram center of mass:"+loss);
-    iter=0;
+    System.out.println("Initial loss for center of mass:"+loss);
+iter=0;
 
-
-    while (error>1.e-12)
-    {
-      iter++;
+    //for (iter=0; iter<nbiter; iter++)
+    while(error>1.e-12)
+    {iter++;
       double [] term=new double [D];
 
-      // compute a quasi-arithmetic mean
       // for all points
       for (k=0; k<n; k++)
       {
         // for all gradF terms to add
+        //  System.out.println(theta.length+" "+thetaset[i].length);
+
         double [] hv=HalfVector(theta, thetaset[k]);
         double [] gf=gradF(hv);
-
+        
         // accumulate the sum of gradients per coordinate
         for (j=0; j<D; j++)
         {
@@ -278,17 +362,17 @@ class JensenShannonCentroidCategoricalDistribution
         term[j]/=(double)n;
       }
 
+      System.out.println("update theta");
+      
       theta=gradG(term);
-      // convert to histogram parameters
       
       res=Theta2Lambda(theta);
-      // update objective function
+      
       double ll=JSsetavg(p, res);
-      // monotone convergence, shall be positive
-      error=loss-ll;
+      error=Math.abs(loss-ll);
       loss=ll;
 
-      System.out.println("Iteration #"+iter+"\tObjective:"+loss+"\t Delta error:"+error);
+      System.out.println(iter+"\t"+loss+"\t Error:"+error);
     }
 
 
@@ -301,25 +385,125 @@ class JensenShannonCentroidCategoricalDistribution
     int d=v.length;
     String s="";
     for (int i=0; i<d; i++) s=s+" "+v[i];
+
     System.out.println(s);
   }
 
 
-  public static void main(String [] args)
-  {
-  int n=15, d=10, i;
-  double [][] pointset=new double [n][d];
- 
-   // int n=2, d=2, i; 
- // double [][] pointset={{0.1,0.9},{0.2,0.8}};
- // double [][] pointset={{0.15,0.85},{0.5,0.5}};
-  	
-    System.out.println("Jensen-Shannon centroids of n="+n+" normalized histograms with d="+d+" bins");
-   
-    for (i=0; i<n; i++) pointset[i]=drawRandom(d);
-    double [] jsc=JSCentroid (pointset);
 
-    System.out.println("Jensen-Shannon centroid:");
-    printVector(jsc);
+  public static void JSCentroidCCCP(double [][] p)
+  {
+   
+    int d=p[0].length;
+    int n=p.length;
+    int i;
+    //double [][] p=new double [n][d];
+   // for (i=0; i<n; i++) p[i]=randomSimplex(d);
+/*
+    double [] theta0=Lambda2Theta(p[0]);
+    double [] theta1=Lambda2Theta(p[1]);
+
+    double hp=h(p[0]);
+    double hfp=-F(theta0);
+    System.out.println(hp+" equals "+hfp);
+
+    double js=JS(p[0], p[1]); 
+    double jf=J(theta0, theta1);
+    System.out.println(js+" equals "+js);
+
+    double [] ttheta0=gradG(gradF(theta0));
+
+    printVector(theta0);
+    printVector(ttheta0);
+
+
+    double [] ttheta1=gradG(gradF(theta1));
+
+    printVector(theta1);
+    printVector(ttheta1);
+*/
+    System.out.println("CCCP"); 
+    double [] centroid= JSCentroid(p);
   }
+
+
+
+  public static  double JSKL (double [] alpha, double [] w, double p[], double[] q)
+  {
+    int k=alpha.length;
+    double res=0;
+    int i;
+    double baralpha=0;
+    for (i=0; i<k; i++) baralpha+=w[i]*alpha[i];
+
+    for (i=0; i<k; i++)
+      res+=w[i]*KL(LERP(alpha[i], p, q), LERP(baralpha, p, q));
+
+
+
+    return res;
+  }
+
+ 
+
+
+  public static  double JSh (double [] alpha, double [] w, double p[], double[] q)
+  {
+    int k=alpha.length;
+    double res=0;
+    int i;
+    double baralpha=0;
+    for (i=0; i<k; i++) baralpha+=w[i]*alpha[i];
+
+    for (i=0; i<k; i++)
+      res+=w[i]*h(LERP(alpha[i], p, q));
+
+
+
+    return h(LERP(baralpha, p, q))-res;
+  }
+
+  public static double [] randomSimplex(int d)
+  {
+    double [] res=new double[d];
+    int i;
+    double csum=0;
+
+    for (i=0; i<d; i++) {
+      res[i]=Math.random(); 
+      csum+=res[i];
+    }
+    for (i=0; i<d; i++) {
+      res[i]/=csum;
+    }
+    return res;
+  }
+
+  public static double [] randomCube(int d)
+  {
+    double [] res=new double[d];
+    int i;
+    double csum=0;
+
+    for (i=0; i<d; i++) {
+      res[i]=Math.random();
+    }
+
+    return res;
+  }
+ 
+
+ 
+
+public static void main(String [] args)
+{int n=100; int d=5; int i;
+
+	double [][] pointset=new double [n][d];
+	for(i=0;i<n;i++) pointset[i]=drawRandom(d);
+		
+	double [] jsc=JSCentroid (pointset);
+}
+ 
+
+ 
 }// end class
